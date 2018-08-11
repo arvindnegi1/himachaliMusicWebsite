de2cab2ad7191ce1e5def7541c2951ca61ee0c77
arvindnegi1 <negi.arvind03@gmail.com>
2018-08-12 00:37:21 +0530

    ui change,improvement in typewriter effect

--- typing.js
+++ typing.js
@@ -2,19 +2,37 @@ var a=0;
 var dat="MUSIC IS LIFE";
 var p;
 var temp1=1;
+var temp2="LOVE";
+var j=13;
+var i;
 function myFunction()
-{if(a<dat.length)
+{
+document.getElementById("typingtext").style.color="blue";
+if(a<dat.length)
 	{document.getElementById("typingtext").innerHTML+=dat.charAt(a);
 		
 	a++;
 
 	setTimeout(myFunction,140);}
 else
+{j=13;
+i=9;
 del();
-
+}
 }
 function del(){
-if(temp1==1)
+document.getElementById("typingtext").innerHTML="MUSIC IS ";
+for(i=9;i<j;i++)
+{document.getElementById("typingtext").innerHTML+=dat.charAt(i);}
+j--;
+if(j==8)
+{var temp3=0;
+var i=0;
+document.getElementById("typingtext").style.color="orange";
+re();
+}
+setTimeout(del,300);
+/*if(temp1==1)
 	{document.getElementById("typingtext").innerHTML="MUSIC IS LIF";
 	temp1++;
 	}
@@ -30,8 +48,36 @@ else if(temp1==4)
 	{document.getElementById("typingtext").innerHTML="MUSIC IS ";
 	temp1++;
 	}
-setTimeout(del,300);
+else if(temp1==5)
+	{document.getElementById("typingtext").innerHTML="MUSIC I ";
+	temp1++;
+	}
+else if(temp1==4)
+	{document.getElementById("typingtext").innerHTML="MUSIC IS ";
+	temp1++;
+	}
+else if(temp1==4)
+	{document.getElementById("typingtext").innerHTML="MUSIC IS ";
+	temp1++;
+	}
+setTimeout(del,300);*/
 }
+function re(){
+document.getElementById("typingtext").innerHTML="MUSIC IS ";
+for(i=0;i<=3;i++)
+	{document.getElementById("typingtext").innerHTML+=temp2.charAt(i);
+	}
+temp3++;	
+	//document.getElementById("typingtext").innerHTML=temp3;
+//if(i>4)
+	//{//document.getElementById("typingtext").innerHTML="MUSIC IS LIFE";
+	
+
+	//}
+//else
+setTimeout(re,1000);
+}
+
 function dropdown()
 	{var x;
 	var temp;
